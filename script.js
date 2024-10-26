902eceee0dfd30ecd9373d1f5a25c6ebcbd5b7bb
Daniil Kostrykin <daniilkostrykin1@gmail.com>
2024-10-26 23:31:02 +0300

    Fixed dates) Refactor script.js and add new script1.js
    - Refactor script.js to improve code readability and remove unnecessary comments and console logs.
    - Add new script1.js file to separate the table loading functionality from the rest of the script.
    
    Closes #123

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
-/*В приложении норм, на сайте нет*/
 document.body.classList.add("dark-theme");
 
 document.addEventListener("DOMContentLoaded", () => {
   const dateInput = document.getElementById("date");
   if (dateInput) {
     const today = new Date().toISOString().slice(0, 10);
-        dateInput.value = today;
+    dateInput.value = today;
   }
 
   updateCurrentTime();
@@ -13,9 +12,25 @@ document.addEventListener("DOMContentLoaded", () => {
 
   updateTotalTrips();
 
-  // Load the table with today's date at the top
-  loadTable();
-  scrollToToday();
+  // Код для переключения темы
+  const themeToggle = document.getElementById("themeToggle");
+
+  themeToggle.addEventListener("click", function () {
+    document.body.classList.toggle("light-theme");
+    document.body.classList.toggle("dark-theme");
+
+    if (document.body.classList.contains("light-theme")) {
+      themeToggle.textContent = "D";
+    } else {
+      themeToggle.textContent = "L";
+    }
+  });
+
+  if (document.body.classList.contains("light-theme")) {
+    themeToggle.textContent = "D";
+  } else {
+    themeToggle.textContent = "L";
+  }
 });
 
 function updateCurrentTime() {
@@ -78,17 +93,15 @@ function submitForm() {
 
   updateTotalTrips();
 
-  // Очистка формы, кроме поля даты
   document.getElementById("groundTransport").value = "";
   document.getElementById("trains").value = "";
   document.getElementById("additionalMetro").value = "";
   document.getElementById("additionalTrains").value = "";
 
-  // Показать сообщение об успешной отправке
   document.getElementById("success-message").style.display = "block";
   setTimeout(() => {
     document.getElementById("success-message").style.display = "none";
-  }, 2000); // Скрыть сообщение через 2 секунды
+  }, 2000);
 }
 
 function calculateTripCost(
@@ -97,10 +110,10 @@ function calculateTripCost(
   additionalMetro,
   additionalTrains
 ) {
-  const groundTransportCost = groundTransport * 57; // Коэффициент для наземного транспорта
-  const trainsCost = trains * 76; // Коэффициент для электричек
-  const additionalMetroCost = additionalMetro * 57; // Коэффициент для доп. поездок на метро
-  const additionalTrainsCost = additionalTrains * 76; // Коэффициент для доп. поездок на электричке
+  const groundTransportCost = groundTransport * 57;
+  const trainsCost = trains * 76;
+  const additionalMetroCost = additionalMetro * 57;
+  const additionalTrainsCost = additionalTrains * 76;
   return (
     groundTransportCost +
     trainsCost +
@@ -115,13 +128,11 @@ function calculateSavings() {
     (total, trip) => total + parseFloat(trip.tripCost),
     0
   );
-  const passCost = 2123; // Цена проездного, заданная разработчиком
+  const passCost = 2123;
   const savings = totalCost - passCost;
 
-  // Используем alert для передачи данных
   alert("Сэкономлено: " + savings + " рублей");
 
-  // Можно использовать console.log для отладки
   console.log("Сэкономлено: " + savings + " рублей");
 }
 
@@ -132,7 +143,7 @@ function generateDates() {
 
   while (startDate <= endDate) {
     dates.push({
-      date: startDate.toISOString().slice(0, 10), // Формат YYYY-MM-DD
+      date: startDate.toISOString().slice(0, 10),
       groundTransport: 0,
       trains: 0,
       additionalMetro: 0,
@@ -145,97 +156,3 @@ function generateDates() {
 
   localStorage.setItem("trips", JSON.stringify(dates));
 }
-
-function loadTable() {
-  if (!localStorage.getItem("trips")) {
-    generateDates();
-  }
-  const trips = JSON.parse(localStorage.getItem("trips")) || [];
-
-  // Сортировка поездок так, чтобы текущая дата была на первом месте
-  const today = new Date().toISOString().split("T")[0];
-  trips.sort((a, b) => {
-    if (a.date === today) return -1;
-    if (b.date === today) return 1;
-    return new Date(b.date) - new Date(a.date);
-  });
-
-  const tableBody = document.querySelector("#tripTable tbody");
-  tableBody.innerHTML = "";
-
-  // Обновляем таблицу
-  trips.forEach((trip) => {
-    const row = document.createElement("tr");
-    if (trip.date === today) {
-      row.id = "today"; // Присваиваем идентификатор строке с текущей датой
-    }
-
-    for (const value of Object.values(trip)) {
-      const cell = document.createElement("td");
-      cell.textContent = value;
-      row.appendChild(cell);
-    }
-
-    tableBody.appendChild(row);
-  });
-
-  // Генерация карточек для мобильных устройств
-  const tableContainer = document.querySelector(".table-container");
-  if (window.innerWidth <= 768) {
-    const cardContainer = document.createElement("div");
-    cardContainer.className = "card-container";
-    trips.forEach((trip) => {
-      const card = document.createElement("div");
-      card.className = "card";
-      card.innerHTML = `
-                <div><span>Дата:</span> ${trip.date}</div>
-                <div><span>Наземный транспорт:</span> ${trip.groundTransport}</div>
-                <div><span>Электрички:</span> ${trip.trains}</div>
-                <div><span>Доп. поездки на метро:</span> ${trip.additionalMetro}</div>
-                <div><span>Доп. поездки на электричке:</span> ${trip.additionalTrains}</div>
-                <div><span>Итого:</span> ${trip.total}</div>
-                <div><span>Стоимость поездок за день:</span> ${trip.tripCost}</div>
-            `;
-      cardContainer.appendChild(card);
-    });
-    tableContainer.appendChild(cardContainer);
-  }
-}
-
-function scrollToToday() {
-  const todayRow = document.getElementById("today");
-  if (todayRow) {
-    todayRow.scrollIntoView({ behavior: "smooth", block: "start" });
-  }
-}
-
-function downloadExcel() {
-  const trips = JSON.parse(localStorage.getItem("trips")) || [];
-  const worksheet = XLSX.utils.json_to_sheet(trips);
-  const workbook = XLSX.utils.book_new();
-
-  XLSX.utils.book_append_sheet(workbook, worksheet, "Trips");
-  XLSX.writeFile(workbook, "trips.xlsx");
-}
-
-document.addEventListener("DOMContentLoaded", function () {
-  const themeToggle = document.getElementById("themeToggle");
-
-  themeToggle.addEventListener("click", function () {
-    document.body.classList.toggle("light-theme");
-    document.body.classList.toggle("dark-theme");
-
-    if (document.body.classList.contains("light-theme")) {
-      themeToggle.textContent = "D";
-    } else {
-      themeToggle.textContent = "L";
-    }
-  });
-
-  // Установите начальный текст кнопки в зависимости от начальной темы
-  if (document.body.classList.contains("light-theme")) {
-    themeToggle.textContent = "D";
-  } else {
-    themeToggle.textContent = "L";
-  }
-});
